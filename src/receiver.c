2d0dc773de14459bece4e5d3c814905084788b56
gign0766 <brewal44@gmail.com>
2015-12-16 10:33:17 +0100

    src

--- src/receiver.c
+++ src/receiver.c
@@ -25,6 +25,58 @@ void to_control(){
   }
 }
 
+void hamming(char c, char out[2]) {
+  out[0] = out[1] = 0;
+  for (int i=0; i < 4; ++i) {
+    if (c & (1 << i))
+      out[1] ^= GMatrix[i] ;
+    if (c & (1 << (i+4)))
+      out[0] ^= GMatrix[i] ;
+  }
+}
+
+void string_of_byte(char s[], const char c) {
+  for (int i = 0 ; i < 8; ++i)
+    s[7-i] = (c & (1 << i)) ? '1' : '0' ;
+  s[8] = '\0' ;
+}
+
+
+void syndrom(){
+  synd[0] = "0000";
+  synd[1] = "0001";
+  synd[2] = "0010";
+  synd[3] = "0011";
+  synd[4] = "0100";
+  synd[5] = "0101";
+  synd[6] = "0110";
+  synd[7] = "0111";
+  synd[8] = "1000";
+  synd[9] = "1001";
+  synd[10] = "1010";
+  synd[11] = "1011";
+  synd[12] = "1100";
+  synd[13] = "1101";
+  synd[14] = "1110";
+  synd[15] = "1111";
+
+
+
+  char out[2] ;
+  char buff[3][10] ;
+
+  for(int i = 0; i < 16;i++){
+    hamming(in,out);
+    string_of_byte(buff[0], in);
+    string_of_byte(buff[1], out[0]);
+    string_of_byte(buff[2], out[1]);
+
+    synd[i] = buff[1] + buff[2]
+    printf("in = %s out = %s\n", buff[0], synd[i]) ;
+
+  }
+}
+
 
 
 void to_print_Matrix() {
@@ -55,6 +107,8 @@ int main() {
   to_print_Matrix();
   printf("\n\n\n");
   to_print_Control();
+  printf("\n\n\n");
+  syndrom();
 
 
 
