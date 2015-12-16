fa915577fb7321226071fea0d085560cae6d2c21
Cédric Berland <zeptoline2@gmail.com>
2015-12-16 11:06:30 +0100

    reveiver

--- src/receiver.c
+++ src/receiver.c
@@ -14,26 +14,47 @@ int GMatrix[4][8] = {{1,1,0,1,1,0,0,0},
 
 
 int GMatrixControl[8][4];
+char* synd[16];
 
 void to_control(){
-    for (int i=0; i < 4; ++i) {
-      G[i] = 0 ;
-      for (int j=0; j < 8; ++j)
-        if (GMatrix[i][j]) G[i] |= (1 << (7-j)) ;
-    }
+  for (int i=0; i < 4; ++i) {
+    for (int j=0; j < 4; ++j)
+      GMatrixControl[j][i] = GMatrix[i][j];
+    for (int k = 4; k < 8; k++)
+      GMatrixControl[k][i] = GMatrix[i][k];
+  }
 }
 
 
 
+void to_print_Matrix() {
+  printf("matrice génératrice\n");
+  for(int i = 0; i < 4; i++) {
+    for (int j = 0; j < 8; j++) {
+      printf (" %d ", GMatrix[i][j]);
+    }
+    printf("\n");
+  }
+}
 
 
-int main(int argc, char*  argv[]) {
+void to_print_Control() {
+  printf("matrice de controle\n");
+  for(int i = 0; i < 8; i++) {
+    for (int j = 0; j < 4; j++) {
+      printf (" %d ", GMatrixControl[i][j]);
+    }
+    printf("\n");
+  }
+}
 
 
+int main() {
 
-
-
-
+  to_control();
+  to_print_Matrix();
+  printf("\n\n\n");
+  to_print_Control();
 
 
 
